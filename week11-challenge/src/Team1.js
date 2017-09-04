3a4d1437b6eecd1a66b933672bc1b7f74ffbb665
TMcGee <teiataylor.m@gmail>
2017-09-04 13:34:33 -0400

    1:34 updates

--- week11-challenge/src/Team1.js
+++ week11-challenge/src/Team1.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function Team1(props){
 return <div className ="assignedList">
-          <h2><button onClick={() => props.Team2HandleClick(props.teamId, true)}>Team2</button> {props.name} <button onClick={() => props.UnAssignedHandleClick(props.teamId, true)}>UnAssigned</button></h2>
+          <h2><button onClick={() =>props.Team2HandleClick(props.teamId, true)}>Team2</button> {props.name} <button onClick={() => props.UnAssignedHandleClick(props.teamId, true)}>UnAssigned</button></h2>
       </div>
 
 
--- week11-challenge/src/UnAssigned.js
+++ week11-challenge/src/UnAssigned.js
@@ -4,7 +4,7 @@ import React from 'react';
 function UnAssigned(props){
   return(
     <div>
-      <h2><button onClick={() => props.Team1HandleClick(props.teamId, true)}>Team1</button> {props.name} <button onClick={() => props.Team2HandleClick(props.teamId, true)}>Team2</button></h2>
+      <h2><button onClick={() =>props.Team1HandleClick(props.teamId, true)}>Team1</button> {props.name} <button onClick={() => props.Team2HandleClick(props.teamId, true)}>Team2</button></h2>
   </div>
   )
   };
